2baa2e751ea9332d154933c033bbf47b78e15378
RomanIShow <ignashov-roman@mail.ru>
2019-10-07 21:37:12 +0300

    Подключение плагина редактора

--- src/app/admin/shared/interfaces.ts
+++ src/app/admin/shared/interfaces.ts
@@ -8,3 +8,12 @@ export interface FbAuthResponse {
   idToken: string;
   expiresIn: string;
 }
+
+
+export interface Post {
+  id?: string;
+  title: string;
+  text: string;
+  author: string;
+  date: Date;
+}
